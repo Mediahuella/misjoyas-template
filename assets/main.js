f52b0ce54c552e5eb22c73255683d70475e39b78
Estefania Sepulveda <estefania@mediahuella.com>
2026-05-19 16:46:55 -0400

    udpate

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,240 @@
-(()=>{var e,t={161(){document.addEventListener("alpine:init",function(){Alpine.store("xSearchBar",{mobileOpen:!1,toggle:function(){this.mobileOpen=!this.mobileOpen},close:function(){this.mobileOpen=!1}}),Alpine.data("xInlineSearch",function(e,t){return{query:"",result:"",cachedResults:{},openResults:!1,loading:!1,_t:null,keyUp:function(){var e=this;clearTimeout(this._t),this._t=setTimeout(function(){var t=e.query.trim();t?e.getSearchResult(t):(e.result="",e.openResults=!1)},300)},focusForm:function(){this.query&&this.result&&(this.openResults=!0)},closeResults:function(){this.openResults=!1},getSearchResult:function(e){var n=this,o=e.toLowerCase().replace(/\s+/g,"-")+"_"+t;if(this.cachedResults[o])return this.result=this.cachedResults[o],void(this.openResults=!0);this.loading=!0;var r="".concat(Shopify.routes.root,"search/suggest?q=").concat(encodeURIComponent(e),"&").concat(encodeURIComponent("resources[type]"),"=").concat(encodeURIComponent("product,collection"),"&").concat(encodeURIComponent("resources[options][fields]"),"=").concat(encodeURIComponent("author,body,product_type,tag,title,variants.barcode,variants.sku,variants.title,vendor"),"&").concat(encodeURIComponent("resources[limit]"),"=").concat(encodeURIComponent(t),"&section_id=predictive-search");fetch(r).then(function(e){return e.text()}).then(function(e){var t=(new DOMParser).parseFromString(e,"text/html").querySelector("#shopify-section-predictive-search");t&&(n.result=t.innerHTML,n.cachedResults[o]=n.result,n.openResults=!0),n.loading=!1}).catch(function(){n.loading=!1})}}})})},620(){}},n={};function o(e){var r=n[e];if(void 0!==r)return r.exports;var s=n[e]={exports:{}};return t[e](s,s.exports,o),s.exports}o.m=t,e=[],o.O=(t,n,r,s)=>{if(!n){var i=1/0;for(l=0;l<e.length;l++){for(var[n,r,s]=e[l],c=!0,a=0;a<n.length;a++)(!1&s||i>=s)&&Object.keys(o.O).every(e=>o.O[e](n[a]))?n.splice(a--,1):(c=!1,s<i&&(i=s));if(c){e.splice(l--,1);var u=r();void 0!==u&&(t=u)}}return t}s=s||0;for(var l=e.length;l>0&&e[l-1][2]>s;l--)e[l]=e[l-1];e[l]=[n,r,s]},o.o=(e,t)=>Object.prototype.hasOwnProperty.call(e,t),(()=>{var e={51:0,360:0};o.O.j=t=>0===e[t];var t=(t,n)=>{var r,s,[i,c,a]=n,u=0;if(i.some(t=>0!==e[t])){for(r in c)o.o(c,r)&&(o.m[r]=c[r]);if(a)var l=a(o)}for(t&&t(n);u<i.length;u++)s=i[u],o.o(e,s)&&e[s]&&e[s][0](),e[s]=0;return o.O(l)},n=self.webpackChunkmisjoyas_template=self.webpackChunkmisjoyas_template||[];n.forEach(t.bind(null,0)),n.push=t.bind(null,n.push.bind(n))})(),o.O(void 0,[360],()=>o(161));var r=o.O(void 0,[360],()=>o(620));r=o.O(r)})();
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main.js"
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+() {
+
+document.addEventListener('alpine:init', function () {
+  Alpine.store('xSearchBar', {
+    mobileOpen: false,
+    toggle: function toggle() {
+      this.mobileOpen = !this.mobileOpen;
+    },
+    close: function close() {
+      this.mobileOpen = false;
+    }
+  });
+  Alpine.data('xInlineSearch', function (type, maxResults) {
+    return {
+      query: '',
+      result: '',
+      cachedResults: {},
+      openResults: false,
+      loading: false,
+      _t: null,
+      keyUp: function keyUp() {
+        var _this = this;
+        clearTimeout(this._t);
+        this._t = setTimeout(function () {
+          var q = _this.query.trim();
+          if (q) {
+            _this.getSearchResult(q);
+          } else {
+            _this.result = '';
+            _this.openResults = false;
+          }
+        }, 300);
+      },
+      focusForm: function focusForm() {
+        if (this.query && this.result) this.openResults = true;
+      },
+      closeResults: function closeResults() {
+        this.openResults = false;
+      },
+      getSearchResult: function getSearchResult(query) {
+        var _this2 = this;
+        var key = query.toLowerCase().replace(/\s+/g, '-') + '_' + maxResults;
+        if (this.cachedResults[key]) {
+          this.result = this.cachedResults[key];
+          this.openResults = true;
+          return;
+        }
+        this.loading = true;
+        var field = 'author,body,product_type,tag,title,variants.barcode,variants.sku,variants.title,vendor';
+        var url = "".concat(Shopify.routes.root, "search/suggest?q=").concat(encodeURIComponent(query), "&").concat(encodeURIComponent('resources[type]'), "=").concat(encodeURIComponent('product,collection'), "&").concat(encodeURIComponent('resources[options][fields]'), "=").concat(encodeURIComponent(field), "&").concat(encodeURIComponent('resources[limit]'), "=").concat(encodeURIComponent(maxResults), "&section_id=predictive-search");
+        fetch(url).then(function (r) {
+          return r.text();
+        }).then(function (text) {
+          var doc = new DOMParser().parseFromString(text, 'text/html');
+          var el = doc.querySelector('#shopify-section-predictive-search');
+          if (el) {
+            _this2.result = el.innerHTML;
+            _this2.cachedResults[key] = _this2.result;
+            _this2.openResults = true;
+          }
+          _this2.loading = false;
+        })["catch"](function () {
+          _this2.loading = false;
+        });
+      }
+    };
+  });
+});
+
+/***/ },
+
+/***/ "./src/css/main.css"
+/*!**************************!*\
+  !*** ./src/css/main.css ***!
+  \**************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		if (!(moduleId in __webpack_modules__)) {
+/******/ 			delete __webpack_module_cache__[moduleId];
+/******/ 			var e = new Error("Cannot find module '" + moduleId + "'");
+/******/ 			e.code = 'MODULE_NOT_FOUND';
+/******/ 			throw e;
+/******/ 		}
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"/assets/main": 0,
+/******/ 			"assets/main": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunkmisjoyas_template"] = self["webpackChunkmisjoyas_template"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["assets/main"], () => (__webpack_require__("./src/js/main.js")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["assets/main"], () => (__webpack_require__("./src/css/main.css")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	
+/******/ })()
+;
